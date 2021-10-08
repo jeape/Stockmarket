8d043bacd03b3b06b2ccc3524b0eeb9c611edb68
Jeanette <jeape@outlook.dk>
2021-10-08 15:20:10 +0200

    feat: Add random generator.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -10,16 +10,34 @@ namespace StockPrices.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        public ILogger<IndexModel> Logger { get; set; }
+        public List<int> AskPrice { get; private set; }
+        public List<int> BidPrice { get; private set; }
+        private int MaxValueBid { get; set; } = 301;
+        private int MaxValueAsk { get; set; } = 401;
 
+        public List<string> Symbols { get; private set; } = new List<string> { "AAPL", "IBM", "MSFT" };
         public IndexModel(ILogger<IndexModel> logger)
         {
-            _logger = logger;
+            Logger = logger;
         }
 
         public void OnGet()
         {
-
+            AskPrice = GenerateRandomNumbers(Symbols.Count(), MaxValueAsk);
+            BidPrice = GenerateRandomNumbers(Symbols.Count(), MaxValueBid);
         }
+
+        public List<int> GenerateRandomNumbers(int length, int maxValue)
+        {
+            var randomGenerator = new Random(0);
+            var randomNumbers = new List<int>();
+            for (int i=0; i <= length; i++)
+            {
+                randomNumbers.Add(randomGenerator.Next(maxValue));
+            }
+            return randomNumbers;
+        }
+
     }
 }
