7a5792a387bb14aafe9d03b624807949f7a15893
Jeanette <jeape@outlook.dk>
2021-10-11 09:57:15 +0200

    feat: Add stock price DTO.

--- DTOs/StockPriceDto.cs
+++ DTOs/StockPriceDto.cs
@@ -7,6 +7,12 @@ namespace StockPrices.DTOs
 {
     public class StockPriceDto
     {
-
+        public string Symbol { get; set; }
+        
+        public decimal BidPrice { get; set; }
+        
+        public decimal AskPrice { get; set; }
+        
+        public DateTimeOffset TransactionTime { get; set; }
     }
 }
